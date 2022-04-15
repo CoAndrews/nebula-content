7ab35c1fefcb4200e8bb08e8777c551ed32cb4ab
CoAndrews <chanelcandrews@gmail.com>
2022-04-14 22:34:24 -0400

    added 0-variables-practice-1.js

---
+++ 1-variables/practice/0-variables-practice-1.js
@@ -0,0 +1,53 @@
+// 1. Declare a new variable and set it equal to the string of your name
+  
+  // 2. Declare a new variable and set it equal to a number 
+   
+  // 3. Set a variable to a boolean value 
+  
+  // 4. Create a variable called carName, assign the value Volvo to it.
+  
+  // 5. Create a variable called num, assign the value 50 to it.
+  
+  // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+
+  
+  // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+
+  const myName = "Chanel";
+
+  console.log(myName)
+
+  let width = "40";
+
+  console.log(width)
+
+  const isOpen = true
+
+  
+
+  console.log(isOpen)
+
+const carName = "Volvo";
+
+console.log(carName)
+
+const num = 50 ;
+
+console.log(num)
+
+
+
+
+const num1 = 5
+
+const num2 = 10;
+
+console.log(num1+num2)
+
+
+
+const num3 = num1 + num2;
+
+
+console.log(num3)
+
---
+++ 1-variables/practice/variable-practice.js
@@ -0,0 +1,17 @@
+//let allows us to reassign myFavoriteShoes, myFavoriteShoes is a Variable and the value is "Nike Jordans"
+let myFavoriteShoes = "adidas Sambas";
+
+console.log(myFavoriteShoes)
+
+myFavoriteShoes = "Nike Jordans"
+
+console.log(myFavoriteShoes)
+
+//cant reassign a constant value
+const birthYear = 1997;
+
+console.log(birthYear)
+
+//dceclaration, variable, value 
+
+// var is being phased out 
