8844c973bf79b3913922ec3305a6ae6411be6e2a
CoAndrews <chanelcandrews@gmail.com>
2022-04-18 22:46:03 -0400

    chanel's homework for variables

--- 1-variables/assignments/variable-hw.js
+++ 1-variables/assignments/variable-hw.js
@@ -1,7 +1,29 @@
-// Create a .js file that satisfies assignment below 
-// Copy the final assignment text into this assignment to submit
-// For each exercise you'll want to log the result to the console to check your work
+//Chanel Andrews
+//i used the practice format to style the variables in a more legible way
 
 //1. Create a set of 6 variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const dogName = "Khaleesi"
+const dogBreed = "Pitt Bull"
+const dogWeight = "59 pounds"
+const dogColor = "Brindle"
+const dogEyeColor = "Brown"
+const dogBirthYear = "2007"
 
+const myDog = `${dogName} ${dogWeight}
+${dogColor}, ${dogBreed}
+${dogEyeColor}, ${dogBirthYear}`
+
+console.log(myDog)
 //2. Create a set of 6 variables that define a vehicle. Must be at least 6 variables.
+const vehicleColor = "Blue"
+const vehicleBrand = "Jaguar"
+const vehicleNickname = "Lightening"
+const vehicleFuel = "Hybrid"
+const vehicleSize = "Coupe"
+const vehicleWheelDrive = "All-Wheel Drive"
+
+const myVehicle = `${vehicleColor}, ${vehicleNickname} 
+${vehicleBrand}, ${vehicleWheelDrive}
+${vehicleSize}, ${vehicleFuel}`
+
+console.log(myVehicle)
