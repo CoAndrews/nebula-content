d5edc9205b4580624c5e862113f37437c314475d
CoAndrews <chanelcandrews@gmail.com>
2022-04-18 21:27:14 -0400

    1-numbers-practice-basics.js

---
+++ 2-numbers/practice/1-numbers-practice-basics.js
@@ -0,0 +1,50 @@
+// Declare a variable and assign it to a numeric value
+let x = 100
+
+// Double that value and re-assign it to the previous variable
+
+x *= 2
+
+// Check it worked by logging to the console
+
+console.log(x)
+
+// Create two numbers, assign them to variables and add them together
+
+let y = 4
+
+let z = 40 
+
+console.log(y + z)
+// Check it worked by logging to the console 
+
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+
+let alpha = 20
+let beta = 300
+let gamma = 57
+
+// Check it worked by logging to the console
+console.log(beta - gamma - alpha)
+// Create two even numbers, assign them to variables and divide one from the other
+
+let delta = 10
+let epsilon = 60
+
+
+// // Check it worked by logging to the console
+console.log(epsilon / delta)
+// Create two odd numbers, assign them to variables and multiply one from the other
+
+let zeta = 11
+let theta = 21
+// Check it worked by logging to the console
+console.log(zeta * theta)
+// Create a variable and log it's remainder, if divided by 3, to the console 
+
+let kappa = 9
+
+console.log(kappa % 3)
+// Answer in plain text, what is modulo especially useful for?
+
+// remainders of division,  finding out reminders of odd numbers that arent easily divisible into other whole numbers
