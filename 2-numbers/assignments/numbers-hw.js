1cb7bdbf0281faaaf119efe080eb6650959d8916
CoAndrews <chanelcandrews@gmail.com>
2022-04-20 22:43:27 -0400

    chanel's numbers hw.js

---
+++ 2-numbers/assignments/numbers-hw.js
@@ -0,0 +1,45 @@
+// Remeber to console.log() to debug your code and check your solutions!
+
+// Declare a variable and assign it to a numeric value
+
+let alpha = 40
+// Half that value and re-assign it to the previous variable 
+alpha /= 2
+
+
+console.log(alpha)
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+let beta = -9
+let eta = -5
+
+console.log(beta - eta)
+// Create three numbers, assign them to variables and multiply them together 
+let a = 3
+let b = 90
+let c = 2.5
+console.log(a * b * c)
+// Create two even numbers, assign them to variables and divide one from the other 
+let theta = 10
+let zeta = 50
+console.log(zeta / theta)
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+let odd1 = 11
+let odd2 = 17
+console.log(odd2 % odd1)
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+console.log(Math.min(10,20,30,40,50))
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+console.log(Math.max(-1,-2,-3,-4,-5))
+// Create a variable and find it's remainder, if divided by 2
+let bees = 507
+console.log(bees % 2)
+// find the square root of 144 
+console.log(Math.sqrt(144))
+// find the value of 3 to the power of 7 
+console.log(Math.pow(3, 7))
+// find the value of 73.2 rounded 
+console.log(Math.round(73.2))
+// find the rounded-down value of 9.9 
+console.log(Math.floor(9.9))
+// find the absolute value of -1231
+console.log(Math.abs(-1231))
